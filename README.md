9501f12341b08d4ab0cb0df73ab1de98ed09870a
Stanislav Strizhkov <d1d2c0d1@gmail.com>
2023-08-30 10:00:52 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# README for install.sh
+
+## 🚀 Introduction
+
+Welcome to the documentation for the install.sh script! This script provides a straightforward way to set up your Debian 11 server with essential components like Docker, Docker Compose, Nginx, and more.
+
+## 🛠 Features
+
+Here's a rundown of what the script does:
+
+- **System Update**: Ensures your system is up-to-date.
+- **Essential Tools Installation**: Installs tools like nano, and sudo.
+- **Nginx Setup**: Installs and sets up Nginx for web serving and reverse proxying.
+- **FTP Configuration**: Allows you to set up FTP for easier file transfers.
+- **Docker & Docker-Compose Installation**: Lets you run containerized applications.
+- **Certbot Installation**: A tool to obtain free SSL certificates for your domains.
+- **NVM & Node.js Setup**: Installs Node Version Manager and the latest version of Node.js.
+- **GIT & GIT-Core**: Installs GIT and GIT-Core for use systems of version controll
+- **User Configuration**: Creates a new user with specific privileges and access.
+
+## 🖥 Requirements
+
+- A fresh Debian 11 or 10.3 installation.
+- Root or sudo access.
+
+## 📖 Usage
+
+1. **Download and Execute the Script**:
+```bash
+wget https://path-to-your-repository/install.sh && chmod +x install.sh && ./install.sh
+```
+
+2. **Follow the Prompts**: The script will ask you for a username, password, and email. Make sure to provide valid inputs!
+
+## 🚧 Important Notes
+
+- Always back up your data before running scripts like this, especially in production environments.
+- The FTP setup allows access only to the home directory of the specified user.
+- For security reasons, it's advised not to use the root user for daily tasks. Use the newly created user.
+
+## 🌐 Certbot and SSL
+
+After running the script, if you wish to obtain SSL certificates for your domains:
+
+1. Navigate to your Nginx sites-enabled directory.
+2. Add your domain configurations.
+3. Run Certbot and follow the prompts.
+
+## 📝 Contribution & Feedback
+
+Feel free to fork this repository, make your changes, and submit a pull request if you think you've made improvements. Feedback is always welcome!
+
+## 📜 License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
+
+## ✅ Conclusion
+
+We hope this script makes your server setup process more comfortable and faster. Cheers to more streamlined development and deployment! 🥂
+
+Remember, always test scripts in a safe environment before deploying them into production. Happy coding! 🎉
---
+++ install.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+# Detecting the OS version
+OS_VERSION=$(lsb_release -sr)
+
+if [ "$OS_VERSION" != "10.3" ] && [ "$OS_VERSION" != "11" ]; then
+    echo "This script is designed for Debian 10.3 or 11 only!"
+    exit 1
+fi
+
+# Check if the user is root or if sudo is available
+if [ "$(id -u)" -ne 0 ]; then
+    if ! command -v sudo &> /dev/null; then
+        echo "Error: sudo is not installed and you are not root."
+        echo "Please run this script as root or install sudo."
+        exit 1
+    fi
+else
+    if ! command -v sudo &> /dev/null; then
+        apt update
+        apt install -y sudo
+    fi
+fi
+
+# Updating the system
+sudo apt update && sudo apt upgrade -y
+
+# Installing common utilities
+sudo apt install -y wget nano sudo
+
+# Setting up a new user
+read -p "Enter the new username: " USERNAME
+read -s -p "Enter the password: " PASSWORD
+echo
+read -p "Enter your email for the ssh key: " EMAIL
+echo
+sudo adduser $USERNAME --gecos "" --disabled-password
+echo "$USERNAME:$PASSWORD" | sudo chpasswd
+
+# Set up sudo privileges
+echo "$USERNAME ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers
+
+# Setting up FTP
+sudo apt install -y vsftpd
+sudo cp /etc/vsftpd.conf /etc/vsftpd.conf.bak
+echo "chroot_local_user=YES" | sudo tee -a /etc/vsftpd.conf
+echo "user_sub_token=$USER" | sudo tee -a /etc/vsftpd.conf
+echo "local_root=/home/$USER" | sudo tee -a /etc/vsftpd.conf
+sudo systemctl restart vsftpd
+
+# Installing Nginx
+sudo apt install -y nginx
+sudo systemctl enable nginx
+sudo systemctl start nginx
+
+# Installing Docker
+if ! command -v docker &> /dev/null; then
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+    rm get-docker.sh
+fi
+
+# Allow the non-root user to use Docker
+sudo usermod -aG docker $USERNAME
+
+# Installing Docker Compose
+if ! command -v docker-compose &> /dev/null; then
+    sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+fi
+
+# Installing Certbot
+sudo apt install -y certbot python3-certbot-nginx
+
+# Installing nvm and Node.js (latest version)
+if [ "$OS_VERSION" == "10.3" ]; then
+    # Installation steps specific to Debian 10.3
+    sudo apt install -y curl
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+else
+    # Installation steps specific to Debian 11
+    sudo apt install -y curl
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+fi
+
+# Source nvm to use it immediately
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+nvm install node
+
+# Installing Git and its tools
+if ! [ -x "$(command -v git)" ]; then
+    sudo apt install -y git git-core
+fi
+
+# Generate SSH Key
+ssh-keygen -t rsa -b 4096 -C "$EMAIL"
+
+# End message
+echo -e "\e[32m✔ Installation completed successfully!\e[0m"
